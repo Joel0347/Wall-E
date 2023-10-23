65a841dd84773617b593918c7cbd9be24f38a5ec
Joel0347 <joelat0307@gmail.com>
2023-10-22 22:32:00 -0400

    first types

---
+++ Wall-E/Main.cs
@@ -0,0 +1,36 @@
+namespace Wall_E;
+
+public class Main 
+{
+    public static void Parsing(string s) {
+        if (Point.IsPoint(s)) {
+            Point.Eval(s);
+            return;
+        }
+
+        if (Line.IsLine(s)) {
+            Line.Eval(s);
+            return;
+        }
+
+        if (Segment.IsSegment(s)) {
+            Segment.Eval(s);
+            return;
+        }
+
+        if (Ray.IsRay(s)) {
+            Ray.Eval(s);
+            return;
+        }
+
+        if (Circle.IsCircle(s)) {
+            Circle.Eval(s);
+            return;
+        }
+
+        if (Color.IsColor(s)) {
+            Point.Eval(s);
+            return;
+        }
+    }
+}
