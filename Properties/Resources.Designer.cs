65bd55dee71c82b77943bac5bf0f86969a02fcbc
ClaudiaHdezPerez <121737030+ClaudiaHdezPerez@users.noreply.github.com>
2023-11-28 22:26:20 -0600

    merge

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -113,16 +113,6 @@ namespace WallE.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap OIP__1_ {
-            get {
-                object obj = ResourceManager.GetObject("OIP (1)", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap OIP__1_1 {
             get {
                 object obj = ResourceManager.GetObject("OIP (1)1", resourceCulture);
